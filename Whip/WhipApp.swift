5109934922371a35d4d9f14843e5c4e70609c265
pagh2322 <pagh2322@naver.com>
2022-06-22 21:47:02 +0900

    create project

---
+++ Whip/WhipApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhipApp.swift
+//  Whip
+//
+//  Created by peo on 2022/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WhipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
